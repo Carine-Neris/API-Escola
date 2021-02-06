fae048baaa7cd67f03d4731f1b07e5f27cd62324
Carine Neris <carine_neris@hotmail.com>
2021-02-06 20:25:41 -0300

    Criado o serializer dos modelos de Estudante e Curso

--- school/views.py
+++ school/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework import viewsets
+from school.models import Student, Subject
+from serializer import StudentSerializer, SubjectSerializer
 
 
-def student(request):
-    if request.method == 'GET':
-        student = {
-            'id':1, 
-            'nome':'Carine'
-        }
-        return JsonResponse(student)
+class StudentsViewSet(viewsets.ModelViewSet):
+    """Exibe todos os alunos"""
+    queryset = Student.objects.all()]
+    serializer_class = StudentSerializer
+
+
+class SubjectsViewSet(viewsets.ModelViewSet):
+    """Exibe todos os cursos"""
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
